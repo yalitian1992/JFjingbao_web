5af0ba82e86d20697fbdbc109c3f4f0dfb4d0f53
mjing <yaomin.jing@ihaveu.net>
2017-11-07 10:54:07 +0800

    start

---
+++ js/index.js
@@ -0,0 +1,83 @@
+$(function () {
+
+
+    new Swiper('#swiper-container-1', {
+        direction: 'vertical',
+        pagination: '.swiper-pagination',
+        paginationBulletRender: function () {
+            return '';
+        },
+        onTouchMove: function(swiper,direction){
+
+        },
+        onTouchEnd: function (swiper) {
+            
+        },
+
+        onTransitionEnd: function(swiper){
+        },
+        //滑动前
+        onSlideChangeStart: function(swiper){
+
+        },
+        //滑动后
+        onSlideChangeEnd: function (swiper) {
+        }
+    })
+
+    new Swiper('#swiper-container-2', {
+        direction: 'horizontal',
+        paginationBulletRender: function () {
+            return '';
+        },
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        effect: 'coverflow',
+        slidesPerView: 3,
+        centeredSlides: true,
+        coverflow: {
+            rotate: 0,
+            stretch: -180,
+            depth: 60,
+            modifier: 2,
+            slideShadows: false
+        },
+
+
+    })
+
+
+    var myswiper = new Swiper('#swiper-container-3', {
+        direction: 'horizontal',
+        paginationBulletRender: function () {
+            return '';
+        },
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+    })
+
+    //µÚÈýÆÁµÄhover
+    $('.little-title-1').on('click', function () {
+        $('#swiper-container-3').css({'visibility': 'visible'});
+        myswiper.initialSlide = 1;
+    });
+
+    $('.little-title-2').on('click', function () {
+        $('#swiper-container-3').css({'visibility': 'visible'});
+        myswiper.initialSlide = 2;
+    });
+
+    $('.little-title-3').on('click', function () {
+        $('#swiper-container-3').css({'visibility': 'visible'});
+        myswiper.initialSlide = 3;
+    });
+
+    $('.little-title-4').on('click', function () {
+        $('#swiper-container-3').css({'visibility': 'visible'});
+        myswiper.initialSlide = 4;
+    });
+
+    $('#close').on('click', function () {
+        $('#swiper-container-3').css({'visibility': 'hidden'});
+    });
+})
