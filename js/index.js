225856169db3c0bb66b0bcce7c0e713860ad7589
yali.tian <yali.tian@iuzuan.com>
2017-11-15 16:56:39 +0800

    vue

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
     new Swiper('#swiper-container-1', {
         direction: 'vertical',
         pagination: '.swiper-pagination',
@@ -25,11 +24,14 @@ $(function () {
         }
     })
 
+  // 轮播图  第六屏
     new Swiper('#swiper-container-2', {
         direction: 'horizontal',
         paginationBulletRender: function () {
             return '';
         },
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         effect: 'coverflow',
@@ -42,9 +44,47 @@ $(function () {
             modifier: 2,
             slideShadows: false
         },
-
-
     })
+
+  //  轮播  第五屏
+  new Swiper('#swiper-container-5', {
+    direction: 'horizontal',
+    paginationBulletRender: function () {
+      return '';
+    },
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    effect: 'coverflow',
+    slidesPerView: 3,
+    centeredSlides: true,
+    coverflow: {
+      rotate: 0,
+      stretch: -180,
+      depth: 60,
+      modifier: 2,
+      slideShadows: false
+    },
+  })
+
+  // 第二屏手风琴
+
+  $(".feature ul li").on("mouseenter", function(){
+    $(this).siblings().addClass("active")
+    $(this).addClass("hover")
+  })
+
+  $(".feature ul li").on("mouseleave", function(){
+    $(this).siblings().removeClass("active")
+    $(this).removeClass("hover")
+  })
+
+  // 第四屏 卡牌切换
+  $(".card-logo").on("click",function(){
+    var idx = $(".card-logo").index(this);
+    $(".card-content ul:nth-child("+ (idx+1) +")").addClass("active").siblings().removeClass("active");
+  })
+
+
 
 
     var myswiper = new Swiper('#swiper-container-3', {
@@ -55,6 +95,8 @@ $(function () {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
     })
+
+
 
     //µÚÈýÆÁµÄhover
     $('.little-title-1').on('click', function () {
